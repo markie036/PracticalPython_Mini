bfc6993e470bbdb853d09ec7a7fd66aa20de5dce
Mark Harrison <mharrisondesign@mail.com>
2018-01-27 12:23:24 +0000

    Test 2 fails as required

--- byotest.py
+++ byotest.py
@@ -14,4 +14,28 @@ test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2);
 #THIS TEST WORKS. Error message is Expected 2, got 0.
 
 
+#TEST 2
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0}, but got {1}".format(a, b)
+    
+test_not_equal(1,1);
+#TEST 2 ends
+
+"""
+#TEST 3
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection,item)
+    
+test_is_in([1,2,3,4],2)
+#TEST 3 ends
+
+
+def test_not_in(collection, item):
+    assert item not in collection, "{0} does contain {1}".format(collection,item)
+
+test_not_in([1,2,3,4],3)
+
+"""
+
+
 print("All tests so far have passed");
